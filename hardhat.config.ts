e7f218eeaccd38dd2cbd2c7077da32f26bb2bd6f
wjsxqs <xsmlcp@163.com>
2021-05-27 20:00:21 +0800

    Update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,26 @@
 import { HardhatUserConfig } from 'hardhat/config';
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-gas-reporter';
+import 'hardhat-contract-sizer';
+import 'hardhat-typechain';
 
-import "hardhat-gas-reporter";
-import "hardhat-contract-sizer";
+require('dotenv').config();
+
+const {
+  INFURA_API_KEY,
+  ETH_MNEMONIC,
+  ETH_PRIVATE_KEY
+} = process.env;
+
+const MNEMONIC = ETH_MNEMONIC || '';
+const PRIVATE_KEY = ETH_PRIVATE_KEY || '';
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   solidity: {
-    version: '0.7.4',
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
@@ -18,22 +29,35 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [PRIVATE_KEY],
+      gas: 5000000,
+      gasPrice: 50000000000 // 50Gwei
+    },
     rinkeby: {
-      url: 'https://mainnet.infura.io/v3/<projectId>',
-      accounts: []
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [PRIVATE_KEY]
+    },
+    hardhat: {
+      chainId: 1337
     },
   },
   gasReporter: {
-    enabled: true,
+    enabled: false
   },
   etherscan: {
     apiKey: ''
   },
   contractSizer: {
     alphaSort: true,
-    runOnCompile: true,
-    disambiguatePaths: false,
+    runOnCompile: false,
+    disambiguatePaths: false
+  },
+  typechain: {
+    outDir: 'types',
+    target: 'ethers-v5'
   }
-}
+};
 
 export default config;
